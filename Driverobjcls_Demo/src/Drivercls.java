ac98b7842e4d668c7e7b15e35f874a2205cedb87
vaagdevi <vaagdevi@DESKTOP-S57FKC3>
2022-12-23 01:26:07 -0800

    Driver Object Class2

---
+++ Driverobjcls_Demo/src/Drivercls.java
@@ -0,0 +1,13 @@
+
+public class Drivercls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Objcls obj;
+		obj=new Objcls();
+		obj.display();
+
+	}
+
+}
---
+++ Driverobjcls_Demo/src/Objcls.java
@@ -0,0 +1,9 @@
+
+public class Objcls {
+	void display() {
+		
+		System.out.println("welcome to vaagdevi");
+	}
+	
+
+}
